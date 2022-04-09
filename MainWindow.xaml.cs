0299539c076c2ed4953fd23c9bff7361a98c03ef
Andrew Cooper <andrewscooper18@gmail.com>
2022-04-09 17:44:04 +0800

    Recursive Bubble

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -20,9 +20,67 @@ namespace FunWithAlgorithms
     /// </summary>
     public partial class MainWindow : Window
     {
+        public List<int> MyList = new List<int>();
         public MainWindow()
         {
             InitializeComponent();
+
+            Random rnd = new Random();
+
+            for (int i = 0; i < 1000; i++)
+            {
+                MyList.Add(rnd.Next(0, 1000));
+            }
+            updateTextBox();
+          
         }
+
+        private void updateTextBox()
+        {
+            ListTextBox.SelectAll();
+
+            ListTextBox.Selection.Text = "";
+            ListTextBox.AppendText("");
+            for (int i = 0; i < MyList.Count; i++)
+            {
+                ListTextBox.AppendText(MyList.ElementAt(i) + " ");
+            }
+        }
+
+        //Recursive Bubble Sort
+        private void SortButton_Click(object sender, RoutedEventArgs e)
+        {
+            BubleSort();
+        }
+
+        private void MergeSortBtn_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+
+        public void BubleSort()
+        {
+            bool changeMade = false;
+            int tmp;
+            for (int i = 0; i < MyList.Count - 1; i++)
+            {
+                if (MyList[i + 1] < MyList[i])
+                {
+                    tmp = MyList[i + 1];
+                    MyList[i + 1] = MyList[i];
+                    MyList[i] = tmp;
+                    changeMade = true;                
+                }
+            }
+            if (changeMade)
+                BubleSort();
+            else
+            {
+                updateTextBox();
+            }
+        }
+
+
     }
 }
